ecc00a7ebfa0219f5b9b2e489d75b365a5ac7e30
Billy Baney <billybaney1@gmail.com>
2020-02-29 20:01:18 -0600

    Forgot to migrate latest

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./server/server.js');
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-    console.log("\n==Running on 5000==\n");
+    console.log("\n==Running on 4000==\n");
 });
