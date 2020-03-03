f7fce4e438015c178a463fad0dd55ccfd94d5331
Billy Baney <billybaney1@gmail.com>
2020-03-03 08:31:13 -0600

    changed jokes migrations

--- migrations/20200229212407_jokes.js
+++ migrations/20200229212407_jokes.js
@@ -1,7 +1,6 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable("jokes", tbl => {
         tbl.increments();
-        tbl.string("joke_title").notNullable();
         tbl.string("joke_question").notNullable();
         tbl.string("joke_answer").notNullable();
         tbl
